8f0824026b393e921f9e2c4d589a6ba4a85ec624
Dino DiGiulio <deansterling@mac.com>
2014-02-20 14:51:56 -0500

    Cleaned up the code so it is more self documenting

--- JsonInterface/extension.php
+++ JsonInterface/extension.php
@@ -1,5 +1,5 @@
 <?php
-// JsonInterface Extension for Bolt, by Bob den Otter
+// JsonInterface Extension for Bolt, by Bob den Otter & Dino DiGiulio
 
 namespace JsonInterface;
 
@@ -9,9 +9,20 @@ use Symfony\Component\HttpFoundation\Response;
 class Extension extends \Bolt\BaseExtension
 {
 
+    private $limit = 10;
+    
+    private $order = "id";
+    
+    private $notice = "OK";
+    
+    private $records = array();
+    
+    private $content_name = "";
+    
+    private $response_data;
 
     /**
-     * Info block for Sitemap Extension.
+     * Info block
      */
     function info()
     {
@@ -37,100 +48,191 @@ class Extension extends \Bolt\BaseExtension
 
     /**
      * Initialize Sitemap. Called during bootstrap phase.
+     * Sets up the specific routing
      */
     function initialize()
     {
-    
-        // Set up the routes for the sitemap..        
+      
         $this->app->match('/{contenttypeslug}/json/data.{extension}', array($this, 'json'))
             ->assert('extension', '(js)')
             ->assert('contenttypeslug', $this->app['storage']->getContentTypeAssert())
         ;
 
     }
-
+ 
+    /** 
+     * Callback to return JSON data
+     */
     public function json($contenttypeslug)
     {
-		
-        // Make sure we output no extra 'fluff'..
+        
+        //\util::var_dump($this->order);
+        
+        $this->turnOffUnessessaryActions();
+        $this->testAndSetContentName($contenttypeslug);
+        $this->testIsAllowed($contenttypeslug);
+        $this->setWhere();
+        $this->setLimit();
+        $this->setOrder();
+        $this->setResponseData($contenttypeslug);
+        return $this->buildJSONResponse();
+
+    }
+    
+    /**
+     * Set's $notice datamember to a sting indicating issues in assebling data
+     */
+    private function setNotice($notice)
+    {
+        if ($this->notice == "OK") {
+            $this->notice = $notice;
+        } else {
+            $this->notice .= " ".$notice;
+        }
+    }
+    
+    /**
+     * Ensures There is no extra noise created by the JSON assembly
+     */
+    private function turnOffUnessessaryActions()
+    {
         $this->app['extensions']->clearSnippetQueue();
         $this->app['extensions']->disableJquery();
         $this->app['debugbar'] = false;
-
-        $notice = "OK";
-
-		//\util::var_dump($contenttype); 
+    }
+    
+    /**
+     * Test the integrety of the content type and sets the $content_type datamember if valid
+     * sets notice if not
+     * returns boolean
+     */
+    private function testAndSetContentName($contenttypeslug)
+    {
+        if (empty($contenttypeslug)) {
+            $this->setNotice("You should pass a valid contenttype slug.");
+            return false;
+        } 
+        $content_type = $this->app['storage']->getContenttype($contenttypeslug);
+        if (isset($content_type["name"])) {
+	    	$this->content_name = strtolower($content_type["name"]);;
+    	}
         if (!empty($contenttypeslug)) {
-            $contenttype = $this->app['storage']->getContenttype($contenttypeslug);
-            $contentname = strtolower($contenttype["name"]);
-            $is_allowed = isset($this->config["allowed"]) && ($this->config["allowed"] === true || array_key_exists($contentname, $this->config["allowed"])); 
-            if (empty($contenttype) || !$is_allowed) {
-                $notice = "Contenttype '". safeString($contenttypeslug) . "' does not exist or is not allowed.";
-            }
-        } else {
-            $notice = "You should pass a valid contenttype slug.";
+	    	return true; 
+        } 
+        $notice = "Contenttype '". safeString($contenttypeslug) . "' does not exist.";
+        $this->setNotice($notice);
+        return false;
+    }
+    
+    /**
+     * Checks to see that the requested data has been allowed in the config.yml file
+     * Sets the $content_name datamember if valid
+     * sets notice if not
+     * returns boolean
+     */
+    private function testIsAllowed($contenttypeslug)
+    {
+    	$has_allowed = isset($this->config["allowed"]);
+    	$all_allowed = $has_allowed ? $this->config["allowed"] === true : false;
+    	$filter_allowed = $has_allowed ? array_key_exists($this->content_name, $this->config["allowed"]) : false;
+    	
+    	
+        $is_allowed = $all_allowed || $filter_allowed;
+        if ($is_allowed) {
+	    	return true; 
+        } 
+        $notice = "Contenttype '". safeString($contenttypeslug) . "' is not allowed.";
+        $this->setNotice($notice);
+        return false;
+    }
+    
+    /**
+     * Makes use of the url var filter to set the $where datamember with a clause from config.yml if valid
+     * returns void if not
+     */
+    private function setWhere()
+    {
+   
+        $filter_is_set = isset($_GET['filter']) && isset($this->config["allowed"][$this->content_name]["filters"]);
+        if(!$filter_is_set) {
+	        return;
+        } 
+         
+        $filters = $this->config["allowed"][$this->content_name]["filters"];
+        $filter = safeString($_GET['filter']); 
+        $this->where = isset($filters[$filter]) ? $filters[$filter] : array_shift(array_values($filters));
+    }
+    
+    /**
+     * Makes use of the url var limit to set the $limit datamember if valid
+     * returns void if not
+     */
+    private function setLimit()
+    {
+        if (empty($_GET['limit'])) return;
+        $this->limit = intval($_GET['limit']);
+    }
+    
+    /**
+     * Makes use of the url var order to set the $order datamember if valid
+     * returns void if not
+     */
+    private function setOrder(){
+        if (empty($_GET['order'])) return;
+        $this->order = safeString($_GET['order']);
+    }
+    
+    /**
+     * Sets the $reponse_data member with the data to return
+     */
+    private function setResponseData($contenttypeslug)
+    {
+        if($this->notice != "OK"){
+            $this->response_data['notice'] = $this->notice;
+            return;
         }
         
-        if(isset($_GET['filter']) && isset($this->config["allowed"][$contentname]["filters"])){
-	        $filters = $this->config["allowed"][$contentname]["filters"];
-	        $filter = $_GET['filter'];
-	        $where = isset($filters[$filter]) ? $filters[$filter] : array_shift(array_values($filters));
-        } else {
-	        $where = "";
-        }
-
-        if (!empty($_GET['limit'])) {
-            $limit = intval($_GET['limit']);
-        } else {
-            $limit = 10;
-        }
-
-        if (!empty($_GET['order'])) {
-            $order = safeString($_GET['order']);
-        } else {
-            $order = "id";
-        }
-
         $records = $this->app['storage']->getContent(
-                $contenttype['slug'],
-                array('limit' => $limit, 'order' => $order),
-                $dummy, $where
-            );
-            
-        $content = array('notice' => '');
-
-        if (!empty($records) && $notice == "OK") {
-
+                $contenttypeslug,
+                array('limit' => $this->limit, 'order' => $this->order),
+                $dummy, 
+                $this->where
+        );
+		
+        if (!empty($records)) {
+        
             foreach($records as $record) {
-                $content['records'][ $record->id ] = array(
+                $this->response_data['records'][ $record->id ] = array(
                     "values" => $record->values,
                     "taxonomy" => $record->taxonomy,
                     "relation" => $record->relation
                 );
             }
 
-        } else if($notice == "OK") {
+        } else {
 
-            $notice = "No records matched or no records present";
+            $this->setNotice("No records matched or no records present");
 
         }
-
-        $content['notice'] = $notice;
-
+        
+        $this->response_data['notice'] = $this->notice;
+    }
+    
+    /**
+     * Builds the JSON response
+     */
+    private function buildJSONResponse()
+    {
         $headers = array('Content-Type' => 'application/javascript; charset=utf-8');
 
         if (!empty($_GET['callback'])) {
-            $body = sprintf("%s(%s);", safeString($_GET['callback']) , json_encode($content));
+            $body = sprintf("%s(%s);", safeString($_GET['callback']) , json_encode($this->response_data));
         } else {
-            $body = json_encode($content);
+            $body = json_encode($this->response_data);
 
         }
 
-
         return new Response($body, 200, $headers);
-
     }
-
-
 }
 
--- JsonInterface/readme.md
+++ JsonInterface/readme.md
@@ -1,17 +1,28 @@
 JsonInterface
 =============
 
-Activate it in your app/config/config.yml by adding RSSAggregator to the `enabled_extensions` option. 
+This extension allows you to get JSON(p) from Bolt by using use as `/{contenttype}/json/data.js?limit=1&callback=c`.
 
-extension to get JSON(p) from Bolt. use as `/json/pages?limit=1&callback=c`
+Allows for optional url variables limit, order, callback and filter.
 
-Example: `enabled_extensions: [ JSON, your_other_extensions... ]`
+TO GET STARTED
+==============
+
+1. Add the JsonInterface folder to the app/extensions folder
+
+2. Enable your extenision thought the Bolt interface or add it to your app/config/config.yml fill like so: `enabled_extensions: [ JSON, your_other_extensions... ]`
+
+3. Set up the JsonInterface/config.yml so that you allow for json to be displayed for all or specific content types (see config.yml file comments)
+
+4. Optionally ad different filters to the JsonInterface to make different requests on content types (see config.yml file comments)
+
+
 
 This was based off of https://gist.github.com/bobdenotter/6732168
 
 
 ***
 
-[![Flattr this extension](http://api.flattr.com/button/flattr-badge-large.png)](https://flattr.com/submit/auto?user_id=bacbos&url=https://github.com/bacbos/bolt-MenuEditor&title=Menu-Editor for bolt cms&language=php&tags=github&category=software)
+[![Flattr this extension](http://api.flattr.com/button/flattr-badge-large.png)](https://flattr.com/submit/auto?user_id=bacbos&url=https://github.com/DeanoDee/bolt-extension-jsoninterface&title=JsonInterface for bolt cms&language=php&tags=github&category=software)
 
 
